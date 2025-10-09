2642476d98834742c69ba220be8a9d23c0c76207
J Scott Smith <jscsmith@gmail.com>
2025-10-08 23:05:54 -0700

    add wait for interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { SceneCamera } from "./SceneCamera";
 import { FlyBy } from "./FlyBy";
 import { SceneEvents } from "./SceneEvents";
 
-export default function App() {
+function Scene() {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas style={{ background: "#111" }}>
-        <SceneEvents />
-        {/* <Wings /> */}
-        <FlyBy />
-        {/* <Grid args={[100, 100]} /> */}
-        <fog attach="fog" color="#111" near={3} far={8} />
+    <Canvas style={{ background: "#111" }}>
+      <SceneEvents />
+      {/* <Wings /> */}
+      <FlyBy />
+      {/* <Grid args={[100, 100]} /> */}
+      <fog attach="fog" color="#111" near={3} far={8} />
 
-        {/* <OrbitControls /> */}
-        <ambientLight intensity={1.2} color="#d4c2ff" />
-        <directionalLight color="#fffcb0" position={[0, 2, -2]} />
-        <SceneCamera position={[-2, 3, -1]} lookAt={[0, 0, 0]} />
-      </Canvas>
+      {/* <OrbitControls /> */}
+      <ambientLight intensity={1.2} color="#d4c2ff" />
+      <directionalLight color="#fffcb0" position={[0, 2, -2]} />
+      <SceneCamera position={[-2, 3, -1]} lookAt={[0, 0, 0]} />
+    </Canvas>
+  );
+}
+
+const params = new URLSearchParams(window.location.search);
+const initialWaitForInteraction = params.get("waitForInteraction") === "true";
+
+export default function App() {
+  const [wait, setWait] = useState(initialWaitForInteraction);
+  return (
+    <div
+      id="app"
+      onClick={() => {
+        if (wait) {
+          setWait(false);
+          window.history.replaceState({}, "", window.location.pathname);
+        }
+      }}
+    >
+      {wait ? null : <Scene />}
     </div>
   );
 }
